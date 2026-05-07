2953ea0fb0f3a31db206f8432a335173725c7dc3
ange0522-06 <angenierazafimahatratra@gmail.com>
2026-05-07 15:12:11 +0300

    first commit

---
+++ src/Student.vue
@@ -0,0 +1,23 @@
+<script>
+    export default{
+        data(){
+            return{
+                company_name: "Company group",
+                topics : [
+                    {name: 'introduction', duration : 5},
+                    {name: 'theorie', duration : 15},
+                    {name: 'pratique', duration : 65},
+                     ]
+            }
+        }
+    }
+</script>
+
+<template>
+  <h1>Formation vue js - <small>{{ company_name}}</small></h1>
+    <ul>
+        <li v-for="topic in topics">{{ topic.name }} : {{ topic.duration }}</li>
+    </ul>
+</template>
+
+<style scoped></style>
